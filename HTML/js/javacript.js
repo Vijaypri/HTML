61f050e4ff912aba40ccc441839be83952a64aa1
Vijaypri <97949644+Vijaypri@users.noreply.github.com>
2023-09-06 12:24:15 +0530

    HTML code
    assessment test code

---
+++ HTML/js/javacript.js
@@ -0,0 +1,10 @@
+
+const clickButton = document.getElementById('clickButton');
+
+clickButton.addEventListener('click', () => {
+    alert('Hello, world!');
+});
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
